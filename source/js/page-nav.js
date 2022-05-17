ebd73e8b01a0ae7ee710fb2d4c556a73e2831634
Dmitry <supsad@yahoo.com>
2022-05-17 18:48:38 +0700

    added script for open/close nav menu

---
+++ source/js/page-nav.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector(".page-nav");
+const button = document.querySelector(".page-nav__toggle");
+
+menu.classList.remove("page-nav_no-js")
+
+button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (menu.classList.contains("page-nav_closed")) {
+    menu.classList.remove("page-nav_closed");
+    menu.classList.add("page-nav_opened");
+  } else {
+    menu.classList.remove("page-nav_opened");
+    menu.classList.add("page-nav_closed");
+  }
+});
